7d180805844a4cd612913dc547f90e01420deb4f
SAMIR <samirhussain3507@gmail.com>
2026-07-18 20:31:21 +0530

    theme

--- Rest-Countries-API/theme.js
+++ Rest-Countries-API/theme.js
@@ -1,12 +1,18 @@
-const themeChangerBtn = document.querySelector('.theme')
 export default function themeChanger() {
+    const themeChangerBtn = document.querySelector('.theme')
+    const moonIcon = themeChangerBtn.querySelector('i')
+    
     themeChangerBtn.addEventListener('click', () => {
         document.body.classList.toggle('dark')
         
         if(document.body.classList.contains('dark')) {
-            themeChangerBtn.textContent = "Light Mode"
+            themeChangerBtn.querySelector('span').innerText = "Light Mode"
+            moonIcon.classList.remove('ri-moon-line')
+            moonIcon.classList.add('ri-moon-fill')
         }else {
-            themeChangerBtn.textContent = "Dark Mode"
+            themeChangerBtn.querySelector('span').innerText = "Dark Mode"
+            moonIcon.classList.remove('ri-moon-fill')
+            moonIcon.classList.add('ri-moon-line')
         }
     })
 
