75b6a27b64bf2d8497be1189a4998c3aa9c16e82
SAMIR <samirhussain3507@gmail.com>
2026-08-24 20:19:41 +0530

    approx done this project

--- qr-code-generator/script.js
+++ qr-code-generator/script.js
@@ -1,17 +1,21 @@
-const btn = document.querySelector('button')
-const input = document.querySelector("input[name='input']")
-const input_container = document.querySelector('.input-container')
-const output_container = document.querySelector('.output-container')
-const img = document.querySelector('.qr-code')
-const download_btn = document.querySelector('.download')
+const btn = document.querySelector("button");
+const input = document.querySelector("input[name='input']");
+const input_container = document.querySelector(".input-container");
+const output_container = document.querySelector(".output-container");
+const img = document.querySelector(".qr-code");
+const download_btn = document.querySelector(".download");
 
-// const API = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`
 
-btn.addEventListener('click', function() {
-    if(input.value.trim() === '') {
-        console.error("input is empty");
-        return;
-    }
-    console.log(input.value);
-    
-})
+btn.addEventListener("click", function () {
+
+  if(input.value) {
+    const qr_img = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+    img.src = qr_img;
+    output_container.style.display = "flex";
+    input_container.style.display = "none";
+    input.value = "";
+  } else {
+    document.querySelector('.input').style.border = "3px solid crimson"
+  }
+
+});
